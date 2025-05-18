d00d0a882218c5f3fde857bf6d56350a4c04326b
sonyaaa-h <sonya17035@gmail.com>
2025-05-18 15:44:55 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import css from "./Header.module.css";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import LogoutModal from "../Modal/LogoutModal";
-import LogoIcon  from "../../assets/icons/money_guard.svg";
-import ExitIcon  from "../../assets/icons/exit 1.svg";
+import { ReactComponent as LogoIcon } from "../../assets/icons/money_guard.svg";
+import { ReactComponent as ExitIcon }  from "../../assets/icons/exit 1.svg";
 
 export default function Header() {
   const username = useSelector((state) => state.auth.user.name);
