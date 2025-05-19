78cc1b31c7978cd7969fcef2e25d6cc5dfc1b96f
Caviar <forworker137@gmail.com>
2025-05-19 18:29:16 +0300

    fixed private route 3

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
       <Route
         path="/"
-        element={<PrivateRoute element={Layout} redirectTo="/login" />}
+        element={<PrivateRoute component={Layout} redirectTo="/login" />}
       >
         <Route index element={<HomeTab />} />
         <Route path="statistics" element={<StatisticsTab />} />
