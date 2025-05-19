d1aa3301f74adbf4e960d8d4462b59b9025ffbdd
Ivan Makovchuk <macad991@gmail.com>
2025-05-19 22:35:18 +0200

    add app word amd layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Layout from "./Layout";
+import PrivateRoute from "./PrivateRoute";
+import RestrictedRoute from "./RestrictedRoute";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import HomeTab from "./pages/HomeTab/HomeTab";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import StatisticsTab from "./pages/StatisticsTab/StatisticsTab";
+import CurrencyTab from "./pages/CurrencyTab/CurrencyTab";
+import { refreshUser } from "./redux/auth/operations";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <Routes>
+      <Route
+        path="register"
+        element={
+          <RestrictedRoute component={RegistrationPage} redirectTo="/" />
+        }
+      />
+
+      <Route
+        path="login"
+        element={<RestrictedRoute component={LoginPage} redirectTo="/" />}
+      />
+
+      <Route
+        path="/"
+        element={<PrivateRoute component={Layout} redirectTo="/login" />}
+      >
+        <Route index element={<HomeTab />} />
+        <Route path="statistics" element={<StatisticsTab />} />
+        <Route path="currency" element={<CurrencyTab />} />
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/" element={<Layout />} />
     </Routes>
   );
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import ButtonAddTransaction from "./components/ButtonAddTransaction/ButtonAddTransaction";
+import AppBar from "./components/AppBar/AppBar";
 
 const Layout = () => {
   const handleClick = () => {
@@ -8,6 +9,7 @@ const Layout = () => {
 
   return (
     <div>
+      <AppBar />
       <Outlet />
       <ButtonAddTransaction onClick={handleClick} />
     </div>
