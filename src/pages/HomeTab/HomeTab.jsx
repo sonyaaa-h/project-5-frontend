a1e983ce63e92599146e4c457ddc49c4c8ea08f8
Sofiia <sonya17035@gmail.com>
2025-05-19 14:22:57 +0200

    dispatch Home

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTransactions } from "../../redux/transactions/operations";
+import { fetchCategories } from "../../redux/categories/operations";
+
 const HomeTab = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return <div>Home page</div>;
 };
 export default HomeTab;
