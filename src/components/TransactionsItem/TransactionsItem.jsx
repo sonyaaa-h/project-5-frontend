7a5ac543eb98ed04826f9b56b1b92bb8aeb3fa66
Olena_Chovgun <olenkachovgun@gmail.com>
2025-05-20 17:14:17 +0300

    add style for transaction

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,14 +1,21 @@
+import s from "./TransactionsItem.module.css";
+import EditIcon from "../../assets/icon-edit.svg?react";
 const TransactionsItem = ({ date, type, category, comment, sum }) => {
-    return (
-        <div>
-            <ul>
-                <li>{date}</li>
-                <li>{type}</li>
-                <li>{category}</li>
-                <li>{comment}</li>
-                <li>{sum}</li>
-            </ul>
-        </div>
-    );
+  return (
+    <li className={s.wrapperTransaction}>
+      <ul className={s.transaction}>
+        <li>{date}</li>
+        {/* виправити формат відображення дати */}
+        <li>{type}</li>
+        <li>{category}</li>
+        <li>{comment}</li>
+        <li>{sum}</li>
+        <li>
+          <EditIcon className={s.icon} />
+          <button className={s.btnDelete}>Delete</button>
+        </li>
+      </ul>
+    </li>
+  );
 };
 export default TransactionsItem;
