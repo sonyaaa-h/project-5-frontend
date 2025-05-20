3c287397e2f843e144b80d9997aa447593474b48
Olena_Chovgun <olenkachovgun@gmail.com>
2025-05-20 22:40:25 +0300

    transaction item

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,6 +1,26 @@
 import s from "./TransactionsItem.module.css";
 import EditIcon from "../../assets/icon-edit.svg?react";
-const TransactionsItem = ({ date, type, category, comment, sum }) => {
+import { useRef } from "react";
+import {
+  useDispatch,
+  // useSelector
+} from "react-redux";
+import toast from "react-hot-toast";
+import { deleteTransaction } from "../../redux/transactions/operations.js";
+import logo from "../../assets/logo.svg";
+
+const TransactionsItem = ({ id, date, type, category, comment, sum }) => {
+  //пусто
+  const modalRef = useRef(null);
+  const dispatch = useDispatch();
+  //const token = useSelector((state) => state.auth.token);
+
+  const handleDelete = () => {
+    dispatch(deleteTransaction(id));
+    toast.error(`Transaction "${comment}" is deleted!`);
+    modalRef.current.close();
+  };
+
   return (
     <li className={s.wrapperTransaction}>
       <ul className={s.transaction}>
@@ -12,9 +32,28 @@ const TransactionsItem = ({ date, type, category, comment, sum }) => {
         <li>{sum}</li>
         <li>
           <EditIcon className={s.icon} />
-          <button className={s.btnDelete}>Delete</button>
+          <button
+            className={s.btnDelete}
+            onClick={() => modalRef.current.showModal()}
+          >
+            Delete
+          </button>
         </li>
       </ul>
+      {console.log(id, "id")}
+      <dialog ref={modalRef} className={s.modalDel}>
+        <div className={s.modalContent}>
+          <img className={s.logo} src={logo} alt="logo" />
+          <p>
+            Are you sure you want to delete?
+            {/* <span className={s.modalName}>"{name}"</span>? */}
+          </p>
+          <div className={s.modalBtn}>
+            <button onClick={handleDelete}>Delete</button>
+            <button onClick={() => modalRef.current.close()}>Cancel</button>
+          </div>
+        </div>
+      </dialog>
     </li>
   );
 };
