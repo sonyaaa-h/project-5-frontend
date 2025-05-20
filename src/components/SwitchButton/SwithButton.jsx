2d8f592da6f98ecbfdc1431245b8038ebc77f525
Dima <oxxTerm0s@gmail.com>
2025-05-21 01:32:51 +0300

    add basic styles

--- src/components/SwitchButton/SwithButton.jsx
+++ src/components/SwitchButton/SwithButton.jsx
@@ -3,10 +3,19 @@ import Switch from "@mui/material/Switch";
 
 const label = { inputProps: { "aria-label": "Color switch demo" } };
 
-export default function ColorSwitches() {
+export default function ColorSwitches({ isIncome, setIsIncome }) {
+  const handleChange = (event) => {
+    setIsIncome(event.target.checked);
+  };
   return (
     <div>
-      <Switch {...label} defaultChecked color="warning" />
+      <Switch
+        {...label}
+        defaultChecked
+        color="warning"
+        checked={isIncome}
+        onChange={handleChange}
+      />
     </div>
   );
 }
