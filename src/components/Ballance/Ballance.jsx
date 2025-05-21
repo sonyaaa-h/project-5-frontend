92b223325e2733f825618a0f75df977c64fc818a
AnnBalias <annbalias@gmail.com>
2025-05-21 13:14:53 +0300

    ballance

--- src/components/Ballance/Ballance.jsx
+++ src/components/Ballance/Ballance.jsx
@@ -1,6 +1,17 @@
+import { useSelector } from "react-redux";
 import css from "./Ballance.module.css"
 
 const Ballance = () => {
-    return <div>Ballance-блок відображення балансу юзера</div>;
+    const balance = useSelector((state) => state.auth.user.balance);
+
+    return (
+        <section className={css.balance}>
+            <p className={css.title}>Your balance</p>
+            <p className={css.amount}>
+                {Number(balance).toFixed(2)}
+                <span className={css.amountSpan}> UAH</span>
+            </p>
+        </section>
+    );
 };
 export default Ballance;
