c58e886354afbda7e30bbbe1ef9ca8678cd18557
AnnBalias <annbalias@gmail.com>
2025-05-25 14:50:41 +0300

    pagination

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,2 +1,3 @@
 export const selectTransactions = (state) => state.transactions.items;
 export const selectError = (state) => state.transactions.error;
+export const selectPagination = (state) => state.transactions.pageInfo;
