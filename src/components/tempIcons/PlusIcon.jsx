d065279da4db894effec0e090fe7e7a4522ce2ad
Olena_Chovgun <olenkachovgun@gmail.com>
2025-05-27 23:23:40 +0300

    fix css btnadd & addForm

--- src/components/tempIcons/PlusIcon.jsx
+++ src/components/tempIcons/PlusIcon.jsx
@@ -2,17 +2,14 @@ const PlusIconModal = ({ className, ...props }) => (
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
-    width="11"
-    height="10"
+    width="25.45"
+    height="25.45"
+    viewBox="0 0 26 26"
     fill="none"
     {...props}
   >
-    <path
-      stroke="#FCFCFC"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M5.5 2.083v5.834M2.583 5h5.833"
-    />
+    <path d="M13 0.272705V25.7273" stroke="#081222" strokeWidth="2.54545" />
+    <path d="M0.272949 13L25.7275 13" stroke="#081222" strokeWidth="2.54545" />
   </svg>
 );
 
