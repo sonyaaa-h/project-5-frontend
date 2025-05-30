4d5d9d13fbef97fc6b349c2cf84f5fb7d08a561d
Viktoriia <mwa10@ukr.net>
2025-05-30 06:26:20 +0300

    fixed css statistic

--- src/components/PieChartWithPaddingAngle/PieChartWithPaddingAngle.jsx
+++ src/components/PieChartWithPaddingAngle/PieChartWithPaddingAngle.jsx
@@ -3,13 +3,13 @@ import { PieChart, Pie, Cell } from "recharts";
 
 export default function PieChartWithPaddingAngle({ data, total }) {
   return (
-    <PieChart width={241} height={207}>
+    <PieChart width={264} height={264}>
       <Pie
         data={data}
         cx="50%"
         cy="50%"
-        innerRadius={70}
-        outerRadius={100}
+        innerRadius={95}
+        outerRadius={132}
         paddingAngle={0}
         startAngle={0}
         endAngle={360}
@@ -30,6 +30,8 @@ export default function PieChartWithPaddingAngle({ data, total }) {
         textAnchor="middle"
         dominantBaseline="middle"
         fill="#fff"
+        fontSize={18}
+        fontWeight={700}
       >
         ₴ {total}
       </text>
